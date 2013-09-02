5ecfb3a489ca042c2bb5e7591f9b26ff924faf6f
Paige Bayless <prbayless@gmail.com>
2013-09-02 16:25:16 -0700

    Edit the README

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pbtwo::Application.config.secret_token = '2db2d957aee31e84a6d6767eb10c3295c20dbaacc2e0ee17fd1bd561248cac5e0ee5eef96cdd8c8cbc3d954471fa2fdd65fbb0807a1ace83152d9046e7faee75'
